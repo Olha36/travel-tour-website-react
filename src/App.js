41c77756c68c8c66191cae5df837b878e0a59eb3
Olha36 <ipzm222_kov@student.ztu.edu.ua>
2023-10-18 14:18:32 +0300

    add components, css and scss files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './App.css'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Working</h2>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+    </>
   )
 }
 
