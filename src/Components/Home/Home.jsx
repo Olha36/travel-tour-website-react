d5eff105e3fab668587b8ff77aaf52c86ed5e9f7
Olha36 <olhakucheruk36@gmail.com>
2024-01-13 19:37:45 +0200

    add basic info

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,19 +5,25 @@ import video from '../../assets/video1.mp4'
 const Home = () => {
     return (
         <section className="home">
-            <div className="overlay">
-                <video src={video} muted autoPlay loop type="video/mp4"></video>
+            <div className="overlay"></div>
+            <video src={video} muted autoPlay loop type="video/mp4"></video>
 
-                <div className="homeContent container">
-                    <div className="textDiv">
-                        <span className="smallText">
-                            Our Packages
-                        </span>
+            <div className="homeContent container">
+                <div className="textDiv">
+                    <span className="smallText">
+                        Our Packages
+                    </span>
 
-                        <h1 className="homeTitle">Search your holiday</h1>
+                    <h1 className="homeTitle">Search your holiday</h1>
+                </div>
+
+                <div className="cardDiv grid">
+                    <div className="destinationInput">
+                        <label htmlFor="city">Search your destination:</label>
                     </div>
                 </div>
             </div>
+            
         </section>
     )
 }
