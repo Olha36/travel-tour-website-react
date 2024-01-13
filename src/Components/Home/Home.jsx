7b896df86782e9baa9882d8e13e00ed3caff4a21
Olha36 <olhakucheruk36@gmail.com>
2024-01-13 19:21:03 +0200

    add html info in Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './home.scss'
+import video from '../../assets/video1.mp4'
 
 const Home = () => {
     return (
         <section className="home">
             <div className="overlay">
-                <video src=""></video>
+                <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+                <div className="homeContent container">
+                    <div className="textDiv">
+                        <span className="smallText">
+                            Our Packages
+                        </span>
+
+                        <h1 className="homeTitle">Search your holiday</h1>
+                    </div>
+                </div>
             </div>
         </section>
     )
